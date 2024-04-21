98dc8988dcf4b8079fe1a2dffc9e53fddbeec65c
aadilkhan7 <kaadil22@gmail.com>
2024-04-21 11:31:18 -0500

    some more progress

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,29 +1,57 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 from datetime import datetime, timezone, timedelta
 
-app = Flask(__name__, template_folder="templates")   
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
 
-@app.route("/time")
-def time_now_est():
-    current_time = datetime.now()
-    new_timezone = timezone(timedelta(hours=-5))  # Change this to the timezone you want
 
-    # Convert the datetime object to the new timezone
-    new_time = current_time.astimezone(new_timezone)
-    current_time_str = new_time.strftime("%H:%M:%S")
+@app.route('/')
+def index():
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes)
 
-    return f"Hello Aadil! The time is {current_time_str}"
 
-@app.route("/date")
-def date_today():
-    current_date = datetime.now().date()
-    return f"Hello Aadil! The date is {current_date}"
+@app.route('/add', methods=['POST'])
+def add_note():
+    content = request.form['content']
+    new_note = Note(content=content)
+    db.session.add(new_note)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    if request.method == 'POST':
+        note.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', note=note)
+
+
+@app.route('/delete/<int:note_id>', methods=['GET', 'POST'])
+def delete_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
- 
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=7777)
